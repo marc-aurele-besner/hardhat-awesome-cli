d2407ff141cbd81a0e4dee98846050bbd6f91586
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-04-04 00:25:35 -0400

    Improve types

--- src/type-extensions.ts
+++ src/type-extensions.ts
@@ -1,18 +1,13 @@
-// If your plugin extends types from another plugin, you should import the plugin here.
-
-// To extend one of Hardhat's types, you need to import the module where it has been defined, and redeclare it.
 import 'hardhat/types/config'
 import 'hardhat/types/runtime'
 
-import { AwesomeCliEnvironmentField } from './AwesomeCliEnvironmentField'
+import { AwesomeAddressBook } from './AwesomeAddressBook'
 
 declare module 'hardhat/types/config' {
-    // This is an example of an extension to one of the Hardhat config values.
-
     // We extend the UserConfig type, which represents the config as written
     // by the users. Things are normally optional here.
     export interface ProjectPathsUserConfig {
-        newPath?: string
+        cli?: string
     }
 
     // We also extend the Config type, which represents the configuration
@@ -26,9 +21,8 @@ declare module 'hardhat/types/config' {
 }
 
 declare module 'hardhat/types/runtime' {
-    // This is an example of an extension to the Hardhat Runtime Environment.
     // This new field will be available in tasks' actions, scripts, and tests.
     export interface HardhatRuntimeEnvironment {
-        example: AwesomeCliEnvironmentField
+        addressBook: AwesomeAddressBook
     }
 }
