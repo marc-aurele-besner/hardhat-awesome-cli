bc78e6f6d9ea97107831eae4b364c5216e85c438
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-05-13 07:40:08 -0400

    prettier mockContracts/test/

--- src/mockContracts/test/test-Mock-ERC1155Upgradeable.js
+++ src/mockContracts/test/test-Mock-ERC1155Upgradeable.js
@@ -1,27 +1,26 @@
 /*
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
 let mockERC1155Upgradeable;
 let deployer;
 
-describe("MockERC1155UpgradeableUpgradeable", function () {
+describe('MockERC1155UpgradeableUpgradeable', function () {
+    beforeEach(async function () {
+        [deployer] = await ethers.getSigners();
 
-  beforeEach(async function () {
-    [deployer] = await ethers.getSigners();
+        const MockERC1155Upgradeable = await ethers.getContractFactory('MockERC1155Upgradeable');
+        mockERC1155Upgradeable = await MockERC1155Upgradeable.deploy();
+        await mockERC1155Upgradeable.deployed();
+        await mockERC1155Upgradeable.initialize('MockERC1155Upgradeable', 'MOCK', 'https://google.com');
+    });
 
-    const MockERC1155Upgradeable = await ethers.getContractFactory("MockERC1155Upgradeable");
-    mockERC1155Upgradeable = await MockERC1155Upgradeable.deploy();
-    await mockERC1155Upgradeable.deployed();
-    await mockERC1155Upgradeable.initialize('MockERC1155Upgradeable', 'MOCK', 'https://google.com');
-  });
+    it('Should return the name of the token', async function () {
+        expect(await mockERC1155Upgradeable.name()).to.equal('MockERC1155Upgradeable');
+    });
 
-  it("Should return the name of the token", async function () {
-    expect(await mockERC1155Upgradeable.name()).to.equal("MockERC1155Upgradeable");
-  });
-
-  it("Should return the symbol of the token", async function () {
-    expect(await mockERC1155Upgradeable.symbol()).to.equal("MOCK");
-  });
+    it('Should return the symbol of the token', async function () {
+        expect(await mockERC1155Upgradeable.symbol()).to.equal('MOCK');
+    });
 });
 */
--- src/mockContracts/test/test-Mock-ERC20Upgradeable.js
+++ src/mockContracts/test/test-Mock-ERC20Upgradeable.js
@@ -1,69 +1,68 @@
 /*
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
 let mockERC20Upgradeable;
 let deployer;
 
-describe("MockERC20Upgradeable", function () {
+describe('MockERC20Upgradeable', function () {
+    beforeEach(async function () {
+        [deployer, user1, user2] = await ethers.getSigners();
 
-  beforeEach(async function () {
-    [deployer, user1, user2] = await ethers.getSigners();
+        const MockERC20Upgradeable = await ethers.getContractFactory('MockERC20Upgradeable');
+        mockERC20Upgradeable = await MockERC20Upgradeable.deploy();
+        await mockERC20Upgradeable.deployed();
+        await mockERC20Upgradeable.initialize('MockERC20Upgradeable', 'MOCK');
+    });
 
-    const MockERC20Upgradeable = await ethers.getContractFactory("MockERC20Upgradeable");
-    mockERC20Upgradeable = await MockERC20Upgradeable.deploy();
-    await mockERC20Upgradeable.deployed();
-    await mockERC20Upgradeable.initialize('MockERC20Upgradeable', 'MOCK');
-  });
-  
-  it("Should return the name of the token", async function () {
-    expect(await mockERC20Upgradeable.name()).to.equal("MockERC20Upgradeable");
-  });
+    it('Should return the name of the token', async function () {
+        expect(await mockERC20Upgradeable.name()).to.equal('MockERC20Upgradeable');
+    });
 
-  it("Should return the symbol of the token", async function () {
-    expect(await mockERC20Upgradeable.symbol()).to.equal("MOCK");
-  });
+    it('Should return the symbol of the token', async function () {
+        expect(await mockERC20Upgradeable.symbol()).to.equal('MOCK');
+    });
 
-  it("Should mint token and have the right balanceOf", async function () {
-    const amount = 1000;
-    await mockERC20Upgradeable.mint(deployer.address, amount);
+    it('Should mint token and have the right balanceOf', async function () {
+        const amount = 1000;
+        await mockERC20Upgradeable.mint(deployer.address, amount);
 
-    expect(await mockERC20Upgradeable.balanceOf(deployer.address)).to.equal(amount);
-  });
+        expect(await mockERC20Upgradeable.balanceOf(deployer.address)).to.equal(amount);
+    });
 
-  it("Should mint token and have the right totalSupply", async function () {
-    const amount = 1000;
-    await mockERC20Upgradeable.mint(deployer.address, amount);
+    it('Should mint token and have the right totalSupply', async function () {
+        const amount = 1000;
+        await mockERC20Upgradeable.mint(deployer.address, amount);
 
-    expect(await mockERC20Upgradeable.totalSupply()).to.equal(amount);
-  });
+        expect(await mockERC20Upgradeable.totalSupply()).to.equal(amount);
+    });
 
-  it("Should mint token and burn them", async function () {
-    const amount = 1000;
-    await mockERC20Upgradeable.mint(deployer.address, amount);
-    expect(await mockERC20Upgradeable.balanceOf(deployer.address)).to.equal(amount);
+    it('Should mint token and burn them', async function () {
+        const amount = 1000;
+        await mockERC20Upgradeable.mint(deployer.address, amount);
+        expect(await mockERC20Upgradeable.balanceOf(deployer.address)).to.equal(amount);
 
-    await mockERC20Upgradeable.burn(amount);
-    expect(await mockERC20Upgradeable.balanceOf(deployer.address)).to.equal(0);
-  });
+        await mockERC20Upgradeable.burn(amount);
+        expect(await mockERC20Upgradeable.balanceOf(deployer.address)).to.equal(0);
+    });
 
-  it("Should mint token and transfer them", async function () {
-    const amount = 1000;
-    await mockERC20Upgradeable.mint(deployer.address, amount);
-    expect(await mockERC20Upgradeable.balanceOf(deployer.address)).to.equal(amount);
+    it('Should mint token and transfer them', async function () {
+        const amount = 1000;
+        await mockERC20Upgradeable.mint(deployer.address, amount);
+        expect(await mockERC20Upgradeable.balanceOf(deployer.address)).to.equal(amount);
 
-    await mockERC20Upgradeable.transfer(user1.address, amount);
-    expect(await mockERC20Upgradeable.balanceOf(user1.address)).to.equal(1000);
-  });
+        await mockERC20Upgradeable.transfer(user1.address, amount);
+        expect(await mockERC20Upgradeable.balanceOf(user1.address)).to.equal(1000);
+    });
 
-  it("Should mint token and transferFrom them", async function () {
-    const amount = 1000;
-    await mockERC20Upgradeable.mint(deployer.address, amount);
-    expect(await mockERC20Upgradeable.balanceOf(deployer.address)).to.equal(amount);
+    it('Should mint token and transferFrom them', async function () {
+        const amount = 1000;
+        await mockERC20Upgradeable.mint(deployer.address, amount);
+        expect(await mockERC20Upgradeable.balanceOf(deployer.address)).to.equal(amount);
 
-    await mockERC20Upgradeable.approve(user1.address, amount);
-    await mockERC20Upgradeable.connect(user1).transferFrom(deployer.address, user2.address, amount);
-    expect(await mockERC20Upgradeable.balanceOf(user2.address)).to.equal(1000);
-  });
+        await mockERC20Upgradeable.approve(user1.address, amount);
+        await mockERC20Upgradeable.connect(user1).transferFrom(deployer.address, user2.address, amount);
+        expect(await mockERC20Upgradeable.balanceOf(user2.address)).to.equal(1000);
+    });
 });
 */
