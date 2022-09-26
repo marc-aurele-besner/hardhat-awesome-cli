6c76cdb8f3e7e8cff8798b93d43e446838ccd513
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-09-26 08:38:04 -0400

    improve tests

--- test/addressBook.test.ts
+++ test/addressBook.test.ts
@@ -39,6 +39,7 @@ describe('Integration tests', function () {
                     '0x0000000000000000000000000000000000000000',
                     'hardhat',
                     '0x0000000000000000000000000000000000000000',
+                    0,
                     '0x0000000000000000000000000000000000000001',
                     1
                 )
@@ -51,6 +52,7 @@ describe('Integration tests', function () {
                 '0x0000000000000000000000000000000000000000',
                 'hardhat',
                 '0x0000000000000000000000000000000000000000',
+                0,
                 '0x0000000000000000000000000000000000000001',
                 1
             )
@@ -59,6 +61,7 @@ describe('Integration tests', function () {
                 '0x0000000000000000000000000000000000000001',
                 'hardhat',
                 '0x0000000000000000000000000000000000000001',
+                0,
                 '0x0000000000000000000000000000000000000002',
                 2
             )
@@ -70,22 +73,186 @@ describe('Integration tests', function () {
             )
         })
 
-        it('retrieveContract()', function () {
+        it('saveContract() wit extra arguments, then clean then from log', function () {
+            expect(
+                this.hre.addressBook.saveContract(
+                    'MockERC20',
+                    '0x0000000000000000000000000000000000000000',
+                    'hardhat',
+                    '0x0000000000000000000000000000000000000000',
+                    0,
+                    '0x0000000000000000000000000000000000000001',
+                    1
+                )
+            ).to.be.equal(undefined)
+            this.hre.addressBook.cleanContractDeployed('name', 'MockERC20', true, true)
+            expect(this.hre.addressBook.retrieveContract('MockERC20', 'hardhat')).to.be.equal('')
+        })
+
+        it('saveContract() wit extra arguments, and extra record, then clean then from log', function () {
+            expect(
+                this.hre.addressBook.saveContract(
+                    'MockERC20',
+                    '0x0000000000000000000000000000000000000000',
+                    'hardhat',
+                    '0x0000000000000000000000000000000000000000',
+                    0,
+                    '0x0000000000000000000000000000000000000001',
+                    1
+                )
+            ).to.be.equal(undefined)
+            this.hre.addressBook.saveContract(
+                'MockERC20-test',
+                '0x0000000000000000000000000000000000000001',
+                'testnet',
+                '0x0000000000000000000000000000000000000000',
+                1,
+                '0x0000000000000000000000000000000000000001',
+                1
+            )
+            this.hre.addressBook.cleanContractDeployed(
+                'network',
+                'hardhat',
+                true,
+                true
+            )
             expect(this.hre.addressBook.retrieveContract('MockERC20', 'hardhat')).to.be.equal(
-                '0x0000000000000000000000000000000000000000'
+                ''
             )
         })
 
-        it('retrieveContractObject()', function () {
-            const retrieveContractObject: IAddressDetails | undefined = this.hre.addressBook.retrieveContractObject(
+        it('3x saveContract() wit extra arguments, then clean then from log', function () {
+            this.hre.addressBook.saveContract(
+                'MockERC20',
+                '0x0000000000000000000000000000000000000000',
+                'testnet',
+                '0x0000000000000000000000000000000000000000',
+                0,
+                '0x0000000000000000000000000000000000000001',
+                1
+            )
+            this.hre.addressBook.saveContract(
+                'MockERC20',
+                '0x0000000000000000000000000000000000000000',
+                'hardhat',
+                '0x0000000000000000000000000000000000000000',
+                0,
+                '0x0000000000000000000000000000000000000001',
+                1
+            )
+            this.hre.addressBook.saveContract(
+                'MockERC20-B',
+                '0x0000000000000000000000000000000000000001',
+                'hardhat',
+                '0x0000000000000000000000000000000000000001',
+                0,
+                '0x0000000000000000000000000000000000000002',
+                2
+            )
+            this.hre.addressBook.cleanContractDeployed(
+                'network',
+                'hardhat',
+                true,
+                true
+            )
+            expect(this.hre.addressBook.retrieveContract('MockERC20', 'hardhat')).to.be.equal(
+                ''
+            )
+            expect(this.hre.addressBook.retrieveContract('MockERC20-B', 'hardhat')).to.be.equal(
+                ''
+            )
+        })
+
+        it('3x saveContract() wit extra arguments, then clean then from lo only one', function () {
+            this.hre.addressBook.saveContract(
+                'MockERC20',
+                '0x0000000000000000000000000000000000000000',
+                'testnet',
+                '0x0000000000000000000000000000000000000000',
+                0,
+                '0x0000000000000000000000000000000000000001',
+                1
+            )
+            this.hre.addressBook.saveContract(
+                'MockERC20',
+                '0x0000000000000000000000000000000000000000',
+                'hardhat',
+                '0x0000000000000000000000000000000000000000',
+                0,
+                '0x0000000000000000000000000000000000000001',
+                1
+            )
+            this.hre.addressBook.saveContract(
+                'MockERC20-B',
+                '0x0000000000000000000000000000000000000001',
+                'hardhat',
+                '0x0000000000000000000000000000000000000001',
+                0,
+                '0x0000000000000000000000000000000000000002',
+                2
+            )
+            this.hre.addressBook.cleanContractDeployed(
+                'network',
+                'testnet',
+                true,
+                true
+            )
+            expect(this.hre.addressBook.retrieveContract('MockERC20', 'testnet')).to.be.equal(
+                ''
+            )
+            expect(this.hre.addressBook.retrieveContract('MockERC20', 'hardhat')).to.be.equal(
+                '0x0000000000000000000000000000000000000000'
+            )
+            expect(this.hre.addressBook.retrieveContract('MockERC20-B', 'hardhat')).to.be.equal(
+                '0x0000000000000000000000000000000000000001'
+            )
+        })
+
+        it('retrieveContract()', async function () {
+            this.hre.addressBook.saveContract(
+                'MockERC20',
+                '0x0000000000000000000000000000000000000001',
+                'hardhat',
+                '0x0000000000000000000000000000000000000000',
+                0,
+                '0x0000000000000000000000000000000000000001',
+                1
+            )
+            expect(await this.hre.addressBook.retrieveContract('MockERC20', 'hardhat')).to.be.equal(
+                '0x0000000000000000000000000000000000000001'
+            )
+        })
+
+        it('retrieveContractObject()', async function () {
+            this.hre.addressBook.saveContract(
+                'MockERC20',
+                '0x0000000000000000000000000000000000000001',
+                'hardhat',
+                '0x0000000000000000000000000000000000000000',
+                0,
+                '0x0000000000000000000000000000000000000001',
+                1
+            )
+            this.hre.addressBook.saveContract(
+                'MockERC20-retrieveContractObject2',
+                '0x0000000000000000000000000000000000000002',
+                'hardhat',
+                '0x0000000000000000000000000000000000000000',
+                0,
+                '0x0000000000000000000000000000000000000001',
+                1
+            )
+            // Wait 1 second to ensure the timestamp is different
+            await new Promise((resolve) => setTimeout(resolve, 1000))
+            const retrieveContractObject: IAddressDetails | null = await this.hre.addressBook.retrieveContractObject(
                 'MockERC20',
                 'hardhat'
             )
 
-            expect(retrieveContractObject).to.not.be.equal(undefined)
+            expect(retrieveContractObject).to.not.be.equal(null)
             if (retrieveContractObject) {
                 expect(retrieveContractObject.name).to.be.equal('MockERC20')
-                expect(retrieveContractObject.address).to.be.equal('0x0000000000000000000000000000000000000000')
+                expect(retrieveContractObject.address).to.be.equal('0x0000000000000000000000000000000000000001')
                 expect(retrieveContractObject.network).to.be.equal('hardhat')
                 expect(retrieveContractObject.blockHash).to.be.equal('0x0000000000000000000000000000000000000001')
                 expect(retrieveContractObject.blockNumber).to.be.equal(1)
